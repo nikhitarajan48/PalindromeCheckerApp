4662cde75060f2f9d32296fce89cf170275c4515
nikhitarajan48 <nikhitarajan48@gmail.com>
2026-03-15 15:49:53 +0530

    Added UC3: user input and palindrome check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
@@ -5,6 +6,7 @@ public class PalindromeCheckerApp {
         System.out.println("Welcome to Palindrome Checker App");
 
         // Application details
+        System.out.println("--- Use Case 1 ---");
         System.out.println("Application Name : Palindrome Checker App");
         System.out.println("Version: 1.0");
 
@@ -12,6 +14,7 @@ public class PalindromeCheckerApp {
         System.out.println("Proceeding to next use case...");
 
         //Use Case 2
+        System.out.println("--- Use Case 2 ---");
         String input = "madam";
 
         boolean isPalindrome = true;
@@ -27,6 +30,29 @@ public class PalindromeCheckerApp {
         // Display result
         System.out.println("Input text: " + input);
         System.out.println("Is it a Palindrome? : " + isPalindrome);
+
+        // Use Case 3
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\n--- Use Case 3 ---");
+
+        System.out.print("\nEnter a string to check: ");
+        String userInput = scanner.nextLine();
+
+        String reversed = "";
+
+        for (int i = userInput.length() - 1; i >= 0; i--) {
+            reversed += userInput.charAt(i);
+        }
+
+        if (userInput.equalsIgnoreCase(reversed)) {
+            System.out.println("The string \"" + userInput + "\" is a palindrome.");
+        } else {
+            System.out.println("The string \"" + userInput + "\" is NOT a palindrome.");
+        }
+
+        scanner.close();
     }
 }
+
 
