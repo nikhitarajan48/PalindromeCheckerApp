85a987932b975bc9de381b62f99b61a861fb70c2
nikhitarajan48 <nikhitarajan48@gmail.com>
2026-03-15 13:47:26 +0530

    Added UC2 functionality

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -10,6 +10,23 @@ public class PalindromeCheckerApp {
 
         System.out.println("\nApplication started successfully.");
         System.out.println("Proceeding to next use case...");
+
+        //Use Case 2
+        String input = "madam";
+
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < input.length() / 2; i++) {
+
+            if (input.charAt(i) != input.charAt(input.length() - i - 1)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Display result
+        System.out.println("Input text: " + input);
+        System.out.println("Is it a Palindrome? : " + isPalindrome);
     }
 }
 
